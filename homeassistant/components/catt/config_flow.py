2d44e1cdd121fe63609b11583dbfb5b70f98cffb
Aubron Wood <aubron@daemon.group>
2021-06-13 09:22:30 -0700

    Updates

--- homeassistant/components/catt/config_flow.py
+++ homeassistant/components/catt/config_flow.py
@@ -1,4 +1,6 @@
 """Config flow for Cast All The Things."""
+import logging
+
 from catt import discovery as cattDiscovery
 
 from homeassistant.core import HomeAssistant
@@ -6,12 +8,14 @@ from homeassistant.helpers import config_entry_flow
 
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 
 async def _async_has_devices(hass: HomeAssistant) -> bool:
     """Return if there are devices that can be discovered."""
     # TODO Check if there are any devices that can be discovered in the network.
     devices = await hass.async_add_executor_job(cattDiscovery.get_cast_devices_info)
-    print(devices)
+    _LOGGER.debug("media scan", devices)
     return len(devices) > 0
 
 
